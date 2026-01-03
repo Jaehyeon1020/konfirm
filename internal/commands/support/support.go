258ec27cc4ce9cde72bbd8848e49a4267aea1887
Jaehyeon1020 <jkjhkk1020@gmail.com>
2026-01-03 23:32:47 +0900

    fix: Usage

--- internal/commands/support/support.go
+++ internal/commands/support/support.go
@@ -12,10 +12,10 @@ func Usage(w io.Writer) {
 	fmt.Fprintln(w, "")
 	fmt.Fprintln(w, "Usage:")
 	fmt.Fprintln(w, "  konfirm kubectl <kubectl args...>")
-	fmt.Fprintln(w, "  konfirm allow add <context>")
+	fmt.Fprintln(w, "  konfirm allow add")
 	fmt.Fprintln(w, "  konfirm allow remove <context>")
 	fmt.Fprintln(w, "  konfirm allow list")
-	fmt.Fprintln(w, "  konfirm allow once <context>")
+	fmt.Fprintln(w, "  konfirm allow once")
 }
 
 func Version(w io.Writer) {
